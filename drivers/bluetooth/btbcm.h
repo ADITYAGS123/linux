dde49fe7f22ea19982bbeb802b0a17fa76dcef08
Adi <you@example.com>
2026-01-22 11:52:16 +0530

    testing new thing

--- drivers/bluetooth/btbcm.h
+++ drivers/bluetooth/btbcm.h
@@ -70,6 +70,8 @@ int btbcm_finalize(struct hci_dev *hdev, bool *fw_load_done, bool use_autobaud_m
 static inline int btbcm_check_bdaddr(struct hci_dev *hdev)
 {
 	printf("Sneha change 1");
+	printf("Adi change 1");
+	printf("Adi changing sneha ");
 	return -EOPNOTSUPP;
 }
 
